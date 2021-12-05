8207e306c158ab9a67fa1500bd36459d638c9b0a
Emmanuel Rapoo <aniemmanuelrapo@gmail.com>
2021-12-05 04:31:48 +0100

    Tinder Back End

--- server.js
+++ server.js
@@ -14,13 +14,11 @@ app.use(Cors())
 //DB config
 const dbURI = 'mongodb+srv://rapoo:rapoo151@cluster0.yydtd.mongodb.net/tinderdb?retryWrites=true&w=majority'
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then((result) => app.listen(8001))
-    .catch((err) => console.log(err))
 
 //API Endpoints
-// app.get('/', (req, res) => {
-//     res.status(200).send("HELLO THIS IS RAPOO TESTING!!!");
-// })
+app.get('/', (req, res) => {
+    res.status(200).send("HELLO THIS IS RAPOO TESTING!!!");
+})
 app.post('/tinder/cards', (req, res) => {
     const dbCard = req.body;
     Cards.create(dbCard, (err, data) => {
@@ -32,7 +30,7 @@ app.post('/tinder/cards', (req, res) => {
     })
 });
 
-app.get('/', (req, res) => {
+app.get('/tinder/cards', (req, res) => {
     Cards.find((err, data) => {
         if(err){
             res.status(500).send(err);
